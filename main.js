9ff27ef16104c042d113247d28559dfd5cb6400c
JJvillegas06 <161662930+JJvillegas06@users.noreply.github.com>
2024-07-18 02:55:09 +0000

    add reinicio and counter

--- main.js
+++ main.js
@@ -3,49 +3,140 @@ const cartasJugador = document.querySelector('.mazo-jugador')
 const btnJugar = document.querySelector('.jugar')
 const eleccionJugador = document.querySelector('.eleccion-jugador')
 const eleccionPc = document.querySelector('.eleccion-pc')
+const resultado = document.querySelector('.resultado')
+const btnReiniciar = document.querySelector('.reiniciar')
+let divContador = document.querySelector('.contador')
 
-
-
-
-let numeros = "A23456789JQK"
+let numeros = "23456789JQKA"
 let pintas = "CPTD"
 let cartas = []
+let cartasGanadasPlayer = []
+let cartasGanadasPc = []
+let cartasEmpate = []
+let Winner
+let contador3 = 0
 
-for (i = 0; i < 20; i ++){
+
+
+
+for (i = 0; i < 20; i++) {
     numero = numeros[Math.floor(Math.random() * numeros.length)]
     pinta = pintas[Math.floor(Math.random() * pintas.length)]
     carta = numero + pinta
     cartas.push(carta)
 }
 
-let cartasJugadorAlt = cartas.slice(0,10)
-let cartasPcAlt = cartas.slice(10,20)
+let cartasPlayer = cartas.slice(0, 10)
+let cartasMaquina = cartas.slice(10, 20)
 
 console.log(cartas)
-console.log(cartasJugadorAlt)
-console.log(cartasPcAlt)
+console.log(cartasPlayer)
+console.log(cartasMaquina)
 
-function juegoAleatorio(){
+function sacarCarta() {
+
+    contador3++
+    divContador.textContent = contador3
     
-    numeroAleatorio1 =  Math.floor(Math.random() * 11)
-    numeroAleatorio2 = Math.floor(Math.random() * 11 )
-    cartashown = cartasJugadorAlt[numeroAleatorio1]
-    cartashown2 = cartasPcAlt[numeroAleatorio2]
+    cartashown = cartasPlayer.shift()
+    cartashown2 = cartasMaquina.shift()
     console.log(cartashown)
     console.log(cartashown2)
+    console.log(cartasPlayer.length)
+    console.log(cartasMaquina.length)
+
+    if (numeros.indexOf(cartashown[0]) == numeros.indexOf(cartashown2[0])) {
+        resultado.innerHTML = "empate"
+        if (pintas.indexOf(cartashown[1]) == pintas.indexOf(cartashown2[1])) {
+            resultado.innerHTML = "empate"
+        } else if (pintas.indexOf(cartashown[1]) > pintas.indexOf(cartashown2[1])) {
+            resultado.innerHTML = "ganaste"
+        } else {
+            resultado.innerHTML = "perdiste"
+
+        }
+    } else if (numeros.indexOf(cartashown[0]) > numeros.indexOf(cartashown2[0])) {
+        resultado.innerHTML = "ganaste"
+    } else {
+        resultado.innerHTML = "perdiste"
+
+    }
+    if(resultado.innerHTML == "empate"){
+        cartasEmpate.push(cartashown)
+        cartasEmpate.push(cartashown2)
+    } else if(resultado.innerHTML == "ganaste"){
+        cartasGanadasPlayer.push(cartashown2)
+        console.log(cartasGanadasPlayer)
+    }else{
+        cartasGanadasPc.push(cartashown)
+        console.log(cartasGanadasPc)
+    }
+
+
+    if(cartasPlayer.length == 0){
+        ganador()
+    }
+
     
-    agregar()
     
 }
 
+function ganador(){
+    let  divResultado = document.querySelector('.ganador')
+    if(cartasGanadasPlayer.length == cartasGanadasPc.length){
+        console.log('empate')
+        Winner = document.createElement('button')
+        Winner.innerHTML = 'empate'
+        divResultado.appendChild(Winner)
+        Winner.classList.add('estilo')
+    }else if(cartasGanadasPlayer.length > cartasGanadasPc.length){
+        alert('ganaste')
+        divContador.innerHTML = 'ganaste'
+        divContador.classList.add('letreroGanador')
+    }else{
+        alert('perdiste')
+        Winner = document.createElement('button')
+        Winner.innerHTML = 'perdiste'
+        divResultado.appendChild(Winner)
+        Winner.classList.add('estilo')
+    }
 
-function agregar (){
+    reiniciar()
     
+}
+
+function agregar() {
     eleccionJugador.innerHTML = `<img src="img/${cartashown}.png">`
     eleccionPc.innerHTML = `<img src="img/${cartashown2}.png">`
-    
-    console.log(cartasJugador) 
+
 }
 
-btnJugar.addEventListener('click', juegoAleatorio ,)
 
+btnJugar.addEventListener('click', sacarCarta)
+
+btnReiniciar.addEventListener('click',reiniciar)
+    
+    
+function reiniciar() {
+    numeros = "23456789JQKA"
+    pintas = "CPTD"
+    cartas = []
+
+    for (i = 0; i < 20; i++) {
+        numero = numeros[Math.floor(Math.random() * numeros.length)]
+        pinta = pintas[Math.floor(Math.random() * pintas.length)]
+        carta = numero + pinta
+        cartas.push(carta)
+    }
+    cartasPlayer = cartas.slice(0, 10)
+    cartasMaquina = cartas.slice(10, 20)
+
+
+
+    console.log(cartas)
+    console.log(cartasPlayer)
+    console.log(cartasMaquina)
+    console.log('hola')
+
+    
+}
